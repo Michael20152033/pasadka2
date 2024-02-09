a161a1c75692f5638e9852e208d7efae8f08ddbb
Michael20152033 <78050962+Michael20152033@users.noreply.github.com>
2024-02-09 13:19:01 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.feedbacks__inner').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    prevArrow: $('.feedbacks-arrows__prev'),
+    nextArrow: $('.feedbacks-arrows__next'),
+    cssEase: 'linear'
+});
